3241adaaa6d782b9409d737498b6a1b51141eeda
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-06 14:49:10 -0400

    ex0 done

---
+++ exercise0.rb
@@ -0,0 +1,32 @@
+#
+# Eg. fav_colours = ...
+#
+# Create an array for each item below that contains the given information:
+#
+# your favourite colours
+# the age of you and your siblings/cousins/friends
+# flip a coin 5 times and record whether or not the result was 'heads'
+# your three favourite performing artists
+# your favourite colours again (this time as symbols instead of strings)
+# Create a hash for each item below that contains the given information:
+#
+# three words and their definitions
+# your favourite movie names and their year of creation
+# three cities of the world and their population
+# the names of your siblings/cousins/friends and their ages
+
+
+fav_colours =['White', 40, 'Bob','flip_result', 'zakir','rakha','ravi', :blue ]
+
+puts(fav_colours)
+
+
+my_hash1 = {  :getone => "first word",:gettwo => "second word",:getthree => "third word",
+          :fav_movie => "amadeus",:fav_movie_year => 1985,
+          :city_1 => 20000,:city_2 => 30000,:city_3 => 40000,
+        :friend => "rana",:friend_age => 45}
+
+
+
+
+puts("\n\n",my_hash1)
