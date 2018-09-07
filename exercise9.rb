eda3f21916f69319353a4e7caccde43dc6d45477
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-07 00:23:46 -0400

    step 5

--- exercise9.rb
+++ exercise9.rb
@@ -1,7 +1,7 @@
 # grocery_list.......
 
 
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon","bananas"]
 
 
 
@@ -10,8 +10,12 @@ def disp_grocery (grocery_list)
         grocery_list.each do |item|
         puts (" * #{item}\n")
         end
+        puts("\nTotal no of item(s) : #{grocery_list.length}")
 
 end
+
+
+# grocery_list.pop
 
 #.........insert a new item
 disp_grocery(grocery_list)
@@ -20,3 +24,32 @@ disp_grocery(grocery_list)
 grocery_list.push << "rice"
 
 disp_grocery(grocery_list)
+
+counter_6 =0
+grocery_list.each do |item|
+    if(item == "bananas")
+      counter_6 = 1
+    end
+end
+
+if(counter_6 == 1)
+puts "You don't need to pick up bananas today"
+else
+puts "\n\n !! You need to pick up bananas  !!\n\n"
+end
+
+
+# .....display second item of the grocery_list
+
+puts ("\nSecond item of the list: #{grocery_list[1]}")
+
+
+# sorting the list
+ sorted_list = grocery_list.sort
+
+
+
+ #...display sorted list with asterisk
+ disp_grocery(sorted_list)
+
+ 
