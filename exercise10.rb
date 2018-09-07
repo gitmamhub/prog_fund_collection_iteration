8538f20d1cb43dba0235975a9d4d1ccef2e4766b
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-07 01:14:08 -0400

    step5

--- exercise10.rb
+++ exercise10.rb
@@ -22,3 +22,28 @@ disp_students(student_list)
 
 student_list[:cohort4] = 43
 disp_students(student_list)
+
+#....using keys method to display cohort names
+
+student_list.each do |key,value|
+  puts ("\n#{key}")
+end
+
+
+#adding 5% of students number of each cohort...
+
+
+ def student_add( students_hash)
+    students_hash.each do |key,value|
+
+          students_hash[key] = (value + (value * 5.0)/100).round(0)
+               #puts "#{value+20}"  #(value * 5.0)/100
+            # puts ("#{key}: #{value} students")
+          end
+ end
+
+
+disp_students(student_list)
+puts "\n"
+student_add(student_list)
+disp_students(student_list)
