594a04821a2a4ef31afe093e81059dc1bd616182
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-09-06 23:56:51 -0400

    step2

--- exercise9.rb
+++ exercise9.rb
@@ -4,8 +4,19 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 
-grocery_list.each do |item|
 
-puts (" * #{item}\n")
+def disp_grocery (grocery_list)
+
+        grocery_list.each do |item|
+        puts (" * #{item}\n")
+        end
 
 end
+
+#.........insert a new item
+disp_grocery(grocery_list)
+
+
+grocery_list.push << "rice"
+
+disp_grocery(grocery_list)
